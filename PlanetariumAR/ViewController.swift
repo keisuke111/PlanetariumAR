c740fc670f0fbbb177c99f0f2aa8099464d8b07f
keisuke111 <kesk.k99@gmail.com>
2019-08-28 14:40:20 +0900

    Initial Commit

---
+++ PlanetariumAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlanetariumAR
+//
+//  Created by 北邑圭佑 on 2019/08/28.
+//  Copyright © 2019 北邑圭佑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
