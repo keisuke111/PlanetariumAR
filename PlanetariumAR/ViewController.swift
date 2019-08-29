1c7eb44e2eae58b8741bf552ba058e1fd72ff814
keisuke111 <kesk.k99@gmail.com>
2019-08-29 15:46:01 +0900

    Update AR機能

--- PlanetariumAR/ViewController.swift
+++ PlanetariumAR/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sceneView.delegate = self
+        
+        // 解析情報を表示
+        #warning("debug")
+        sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
+        sceneView.showsStatistics = true
+        
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        // 平面、垂直面を検知
+        configuration.planeDetection = [.horizontal, .vertical]
+        // 環境光に合わせてレンダリング
+        configuration.isLightEstimationEnabled = true
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
 
 }
 
